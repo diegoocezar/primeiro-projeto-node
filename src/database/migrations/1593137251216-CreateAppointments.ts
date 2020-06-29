f0c8045763a164cfd04a6a8692a67e72dedd2bb4
diegoocezar <diego.bernardino@amedigital.com>
2020-06-29 01:23:37 -0300

    migration to change database field

--- src/database/migrations/1593137251216-CreateAppointments.ts
+++ src/database/migrations/1593137251216-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593137251216
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
