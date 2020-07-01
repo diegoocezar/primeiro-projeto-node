9d07f1dbfbcbbafc985ec019aa516d41fa3e0783
diegoocezar <diego.bernardino@amedigital.com>
2020-07-01 01:11:37 -0300

    create sessions route and authenticate

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  public async execute({
+    email,
+    password,
+  }: Request): Promise<{ user: User; token: string }> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ email });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, 'd01c70666092eb572e69255b9cea8f84', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
