a34880c4dc79d392cdb271284540d08ca20e715d
diegoocezar <diego.bernardino@amedigital.com>
2020-07-29 00:33:42 -0300

    Final módulo 02

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -10,7 +18,7 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('JWT token is missing.');
+    throw new AppError('JWT token is missing.', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -18,8 +26,14 @@ export default function ensureAuthenticated(
   try {
     const decoded = verify(token, authConfig.jwt.secret);
 
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch (err) {
-    throw new Error('Invalid JWT token!');
+    throw new AppError('Invalid JWT token!', 401);
   }
 }
